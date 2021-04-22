f50f7e902d42939bf29f90f3fe75c18d978202bf
armaarm <armaarmsarun@gmail.com>
2021-04-22 16:20:39 +0700

    falseposition

--- src/App.js
+++ src/App.js
@@ -35,18 +35,19 @@ class App extends Component {
         <BrowserRouter>
           <Route exact path="/" component={Home} />
           <Route exact path="/Bisection" component={Bisection} />
-          {/* <Route exact path="/FalsePos" component={FalsePos} />
-          <Route exact path="/OnePoint" component={OnePoint} />
-          <Route exact path="/Secant" component={Secant} />
-          <Route exact path="/NewtonRaphson" component={NewtonRaphson} />
-          <Route exact path="/CramersRule" component={CramersRule} />
-          <Route exact path="/GaussElimination" component={GaussElimination} />
-          <Route exact path="/GaussJordan" component={GaussJordan} />
-          <Route exact path="/ConjugateGradient" component={ConjugateGradient} />
-          <Route exact path="/Cholesky" component={Cholesky} />
-          <Route exact path="/LUDecompose" component={LUDecompose} />
-          <Route exact path="/GaussSeidel" component={GaussSeidel} />
-          <Route exact path="/Jacobi" component={Jacobi} /> */}
+          { <Route exact path="/FalsePos" component={FalsePos} />
+          // <Route exact path="/OnePoint" component={OnePoint} />
+          // <Route exact path="/Secant" component={Secant} />
+          // <Route exact path="/NewtonRaphson" component={NewtonRaphson} />
+          // <Route exact path="/CramersRule" component={CramersRule} />
+          // <Route exact path="/GaussElimination" component={GaussElimination} />
+          // <Route exact path="/GaussJordan" component={GaussJordan} />
+          // <Route exact path="/ConjugateGradient" component={ConjugateGradient} />
+          // <Route exact path="/Cholesky" component={Cholesky} />
+          // <Route exact path="/LUDecompose" component={LUDecompose} />
+          // <Route exact path="/GaussSeidel" component={GaussSeidel} />
+          // <Route exact path="/Jacobi" component={Jacobi} /> 
+        }
         </BrowserRouter>
         </div>
       </div>
