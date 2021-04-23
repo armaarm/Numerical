64a4f7e2b5cd2ae59c73a9a31c9513ea0de7369d
armaarm <armaarmsarun@gmail.com>
2021-04-23 15:11:55 +0700

    onepoint

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ class App extends Component {
           <Route exact path="/Bisection" component={Bisection} />
           <Route exact path="/FalsePos" component={FalsePos} />
           <Route exact path="/NewtonRaphson" component={NewtonRaphson} />
-           {/* <Route exact path="/OnePoint" component={OnePoint} />
-           <Route exact path="/Secant" component={Secant} />
+           <Route exact path="/OnePoint" component={OnePoint} />
+           {/* <Route exact path="/Secant" component={Secant} />
            <Route exact path="/CramersRule" component={CramersRule} />
            <Route exact path="/GaussElimination" component={GaussElimination} />
            <Route exact path="/GaussJordan" component={GaussJordan} />
@@ -46,8 +46,7 @@ class App extends Component {
            <Route exact path="/Cholesky" component={Cholesky} />
            <Route exact path="/LUDecompose" component={LUDecompose} />
            <Route exact path="/GaussSeidel" component={GaussSeidel} />
-           <Route exact path="/Jacobi" component={Jacobi} /> 
-         */}
+           <Route exact path="/Jacobi" component={Jacobi} />  */}
         </BrowserRouter>
         </div>
       </div>
